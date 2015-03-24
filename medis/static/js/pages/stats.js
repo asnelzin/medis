28c6555ae132e2577a8b0a6be37a04b0079d2e0b
Alexander Nelzin <asnelzin@gmail.com>
2015-03-24 16:00:55 +0300

    Another views refactor

--- medis/static/js/pages/stats.js
+++ medis/static/js/pages/stats.js
@@ -7,7 +7,6 @@ $(document).ready(
             var year = $("#id_year option:selected").text();
             for (var idx in data) {
                 var url = "/stats/spec/?speciality=" + data[idx].pk + "&month=" + month + "&year=" + year;
-                console.log(url);
                 $("tr:has(td:contains(" + data[idx].name + ")) td:nth-child(2) a").attr("href", url);
                 $("tr:has(td:contains(" + data[idx].name + ")) td:nth-child(3)").text(data[idx].tickets);
                 $("tr:has(td:contains(" + data[idx].name + ")) td:nth-child(4)").text(data[idx].patients);

aa02cdadd5f28dfd89e1e4bb31b450d329fa7038
asnelzin <asnelzin@gmail.com>
2015-03-25 14:45:44 +0300

    Refactor js

--- medis/static/js/pages/stats.js
+++ medis/static/js/pages/stats.js
@@ -1,30 +1,35 @@
-$(document).ready(
-    function () {
-        var form = $('#filter-form');
+(function() {
 
-        function updateStats(data) {
-            var month = $("#id_month option:selected").attr("value");
-            var year = $("#id_year option:selected").text();
-            for (var idx in data) {
-                var url = "/stats/spec/?speciality=" + data[idx].pk + "&month=" + month + "&year=" + year;
-                $("tr:has(td:contains(" + data[idx].name + ")) td:nth-child(2) a").attr("href", url);
-                $("tr:has(td:contains(" + data[idx].name + ")) td:nth-child(3)").text(data[idx].tickets);
-                $("tr:has(td:contains(" + data[idx].name + ")) td:nth-child(4)").text(data[idx].patients);
-
-            }
+    function ajaxSuccess(data) {
+        var month = $("#id_month").find("option:selected").attr("value");
+        var year = $("#id_year").find("option:selected").text();
+        for (var idx in data) {
+            var url = "/stats/spec/?speciality=" + data[idx].pk + "&month=" + month + "&year=" + year;
+            $("tr:has(td:contains(" + data[idx].name + ")) td:nth-child(2) a").attr("href", url);
+            $("tr:has(td:contains(" + data[idx].name + ")) td:nth-child(3)").text(data[idx].tickets);
+            $("tr:has(td:contains(" + data[idx].name + ")) td:nth-child(4)").text(data[idx].patients);
         }
+    }
 
-        function getMonthStats() {
+    var init = function() {
+        var form = $('#filter-form')
+        form.on('submit', function() {
             $.ajax({
                 url: form.attr('action'),
                 data: form.serialize(),
                 type: form.attr('method'),
-                success: updateStats
+                success: ajaxSuccess
             });
             return false;
-        }
+        });
 
-        form.submit(getMonthStats);
-        getMonthStats();
-    }
-);
+        $('.form-control').change(function(e) {
+            form.submit();
+            return false;
+        });
+
+        form.submit();
+    };
+
+    $(init);
+})();

b5d8b5be64586c08232450b117c6b5f98545cf55
Alexander Nelzin <asnelzin@gmail.com>
2015-03-26 22:50:03 +0300

    Refactor

--- medis/apps/stats/urls.py
+++ medis/apps/stats/urls.py
@@ -1,11 +1,9 @@
 from django.conf.urls import patterns, url
 
-from medis.apps.stats.views import (AjaxMonthStatsView, MonthStatsView,
-                                    SpecialityStatsView)
+from medis.apps.stats.views import (MonthStatsView, SpecialityStatsView)
 
 urlpatterns = patterns(
     '',
     url(r'^month/$', MonthStatsView.as_view(), name='month'),
-    url(r'^month/ajax/$', AjaxMonthStatsView.as_view(), name='month-ajax'),
     url(r'^spec/', SpecialityStatsView.as_view(), name='month-speciality'),
 )
--- medis/apps/stats/views.py
+++ medis/apps/stats/views.py
@@ -6,52 +6,43 @@ from datetime import datetime
 
 from django.core.serializers.json import DjangoJSONEncoder
 from django.http import HttpResponse
-from django.views.generic import FormView, TemplateView, View
+from django.views.generic import TemplateView
 
 from medis.apps.stats.forms import FilterForm
 from medis.apps.stats.models import Speciality, Ticket
 
 
-class AjaxMonthStatsView(View):
+class MonthStatsView(TemplateView):
+    template_name = 'stats/month.html'
+
     def get_form_errors(self, form):
         return dict([(k, [unicode(e) for e in v]) for k, v in form.errors.items()])
 
     def data_to_json(self, context_dict):
         return json.dumps(context_dict, cls=DjangoJSONEncoder)
 
-    def render_json_response(self, json_context='', status=200):
-        return HttpResponse(json_context, content_type='application/json', status=status)
-
-    def render_form_error_json_response(self, form, status=400):
-        json_context = self.data_to_json(self.get_form_errors(form))
-        return HttpResponse(json_context, content_type='application/json', status=status)
-
-    def post(self, request, *args, **kwargs):
-        if request.is_ajax():
-            form = FilterForm(request.POST)
-            if form.is_valid():
-                data = Ticket.objects.get_monthly_stats(form.data['month'], form.data['year'])
-                return self.render_json_response(self.data_to_json(data))
-            else:
-                return self.render_form_error_json_response(form)
-        else:
-            return self.render_json_response('', 404)
-
-
-class MonthStatsView(FormView):
-    form_class = FilterForm
-    template_name = 'stats/month.html'
-
     def get_context_data(self, **kwargs):
         context = super(MonthStatsView, self).get_context_data(**kwargs)
-
         current_date = datetime.now()
         data = Ticket.objects.get_monthly_stats(current_date.month, current_date.year)
-        context.update({
-            'current_date': current_date,
-            'data': data
-        })
+        context['form'] = self.form
+        context['current_date'] = current_date
+        context['data'] = data
         return context
+
+    def get_json_response(self):
+        if self.form.is_valid():
+            data = Ticket.objects.get_monthly_stats(self.form.data['month'], self.form.data['year'])
+            return HttpResponse(self.data_to_json(data), content_type='application/json')
+        json_content = self.data_to_json(self.get_form_errors(self.form))
+        return HttpResponse(json_content, content_type='application/json', status=400)
+
+    def get(self, request, *args, **kwargs):
+        self.form = FilterForm(request.GET)
+        if self.request.is_ajax():
+            return self.get_json_response()
+        context = self.get_context_data()
+        return self.render_to_response(context)
 
 
 class SpecialityStatsView(TemplateView):
